b614310cadf3a70351f166360b659b5925badf27
tvatt <github@vattima.com>
2024-11-16 10:26:14 -0500

    Fixes DatabaseProperties to have ConfigurationProperties annotation

--- src/main/java/net/bricklink/data/lego/ibatis/configuration/DatabaseProperties.java
+++ src/main/java/net/bricklink/data/lego/ibatis/configuration/DatabaseProperties.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +12,7 @@ import java.util.Map;
 @Slf4j
 @Getter
 @Setter
+@ConfigurationProperties
 public class DatabaseProperties {
     private Map<String, Database> databases = new HashMap<>();
 
